74dd6ab0a476a18b75c99a3e98785e8f6efd8fd7
juliex-04 <149920286+juliex-04@users.noreply.github.com>
2023-11-04 23:48:05 -0500

    Update question1_test.py

--- part1/question1_test.py
+++ part1/question1_test.py
@@ -1,7 +1,10 @@
-from question1 import get_city_weather
+from Pregunta1 import get_city_weather
 
 def test_get_city_weather():
 
-  assert get_city_weather("Quito") == "22 degrees and sunny"
+  assert get_city_weather("Quito") == "22 degrees and unknown"
 
   assert get_city_weather("New York") == "14 degrees and rainy"
+
+if __name__ == "__main__":
+    test_get_city_weather()
